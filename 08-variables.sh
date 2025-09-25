f0f21bfecb26799b88f5ff0f4679e89fdc88e4ab
Surya <suryaphaniteja007@gmail.com>
2025-09-25 20:46:39 +0530

    new changes

--- 08-variables.sh
+++ 08-variables.sh
@@ -3,3 +3,6 @@
 echo "all the variables pass to this script: $@"
 echo "all the variables pass to this script: $*"
 echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "who is running the script : $USER"
+echo "Home Direcotry of the user: $HOME"
