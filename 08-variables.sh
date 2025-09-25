297c8bd12065b00eb3693ca7db9190706f92af47
Surya <suryaphaniteja007@gmail.com>
2025-09-25 20:50:17 +0530

    new changes

--- 08-variables.sh
+++ 08-variables.sh
@@ -7,3 +7,5 @@ echo "Current Directory: $PWD"
 echo "who is running the script : $USER"
 echo "Home Direcotry of the user: $HOME"
 echo "Process excute ID: $$"
+sleep 50 &
+echo "PID of the last command ID Is: $!"
