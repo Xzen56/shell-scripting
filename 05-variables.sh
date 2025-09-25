00c0649007f07caf7513ac83e59948d3fbcc97ee
Surya <suryaphaniteja007@gmail.com>
2025-09-25 18:57:36 +0530

    new changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,5 +3,7 @@
 echo "Please enter the pin number"
 read -s PIN_NUMBER #here the pin is avaiable
 
+# -s which hide the entered value whether it may be alphabet or number
+
 
 echo "PIN Entered Is: $PIN_NUMBER"
